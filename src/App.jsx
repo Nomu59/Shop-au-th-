82931a4f759dd7de2ecbdb7a8ae55fbe654ed107
59013-96-10 <59013-96-10@exchange.ad.afpanet>
2023-08-24 09:20:48 +0200

    projet

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,33 @@
-import "./App.css";
 import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
+// import Footer from "./components/Footer";
 import Login from "./Pages/Connexion/Login";
 import Search from "./Pages/Search/Search";
 import Cart from "./Pages/Cart/Cart";
 import Home from "./Pages/Home/Home";
-import Produits from "./components/Produits.jsx"
+import ProduitDetail from "./components/ProduitDetail";
+import Footer from "./components/Footer";
+import "./App.css";
+import "./assets/css/navbar.css";
+import "./assets/css/animation.css";
+import "./assets/font/RobotoCondensed-Regular.ttf";
+import "./assets/font/AQUATIC.ttf";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div className="app">
         <Navbar />
-        <Produits/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Connexion" element={<Login />} />
           <Route path="/Search" element={<Search />} />
           <Route path="/Cart" element={<Cart />} />
+          <Route path="/produit/:id" Component={ProduitDetail} /> // :id pour recup l'id 
         </Routes>
+         <Footer/> 
       </div>
     </>
   );
--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Cart = () => {
   return (
-    <div>Cart</div>
+    <div>
+      <p className="text-3xl font-bold underline">Cart</p>Cart</div>
   )
 }
 
